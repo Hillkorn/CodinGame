acef696c0cb753950f00b678c73aeee76f2132d1
Dennis Schumann <hillkorn@gmail.com>
2016-09-25 14:27:03 +0200

    Backup of ghostbusters.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,6 @@
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Send your busters out into the fog to trap ghosts and bring them home!
@@ -11,10 +12,12 @@ class Player {
     static HashMap<Integer, Ghost> ghosts = new HashMap<>();
     static HashMap<Integer, Buster> busters = new HashMap<>();
     static HashMap<Integer, Buster> enemyBusters = new HashMap<>();
+    static boolean discoveredAll = false;
 
     public static void main(String args[]) {
         toDiscover = new ArrayList<>();
         filloutDiscoveryList();
+        discoveredAll = false;
 
         Scanner in = new Scanner(System.in);
         bustersPerPlayer = in.nextInt(); // the amount of busters you control
@@ -26,6 +29,9 @@ class Player {
 
         // game loop
         while (true) {
+            for (Buster buster : busters.values()) {
+                buster.onGhost = null;
+            }
             for (Buster enemy : enemyBusters.values()) {
                 enemy.vis = false;
             }
@@ -45,86 +51,68 @@ class Player {
             if (lastBusted >= 0) {
 //                System.err.println(ghosts.get(lastBusted));
             }
-            int i = 0;
-            for (Buster enemyBuster : enemyBusters.values()) {
-                if (enemyBuster.gotStunned > 0) {
-                    enemyBuster.gotStunned--;
-                }
+            Set<Integer> busterKeys = busters.keySet();
+            List<Integer> busterKeysSorted = busterKeys.stream().sorted().collect(Collectors.toList());
+            for (Integer busterKey : busterKeysSorted) {
+                String action = getBusterAction(busterKey, myBaseCoords);
+                System.out.println(action);
             }
-            for (Buster buster : busters.values()) {
-                if (buster.stunCD > 0) {
-                    buster.stunCD--;
-                }
-                checkDiscovery(buster.x, buster.y);
-                if (buster.state == 2) {
-                    moveTo(new Coord(0, 0));
-                    continue;
-                }
-                if (buster.state == 1) {
-                    Integer distBase = getDist(buster, myBaseCoords);
-                    System.err.println("DistBase " + distBase);
-                    if (distBase <= BASE_COLLECT_RANGE) {
-                        System.out.println("RELEASE");
-                        continue;
-                    } else {
-                        System.err.println("Move to base " + myBaseCoords);
-                        System.out.println("MOVE " + myBaseCoords.x + " " + myBaseCoords.y);
-//                        moveTo(myBaseCoords);
-                        continue;
-                    }
-                }
-                for (Buster enemyBuster : enemyBusters.values()) {
-                    if (enemyBuster.vis && getDist(buster, enemyBuster) <= STUN_RANGE && enemyBuster.gotStunned == 0) {
-                        System.out.println("STUN " + enemyBuster.id);
-                        buster.stunCD = 20;
-                        enemyBuster.gotStunned = 10;
-                        continue;
-                    }
-                }
-                if (ghosts.isEmpty()) {
-//                    System.err.println("All emtpy");
-//                    System.out.println("MOVE 8000 4500"); // MOVE x y | BUST id | RELEASE
-                    discoverMove(buster);
-                    continue;
-                } else {
-                    Ghost ghost = getNextGhost(buster, ghosts, true);
-                    if (ghost == null) {
-                        System.err.println("NGhost is null");
-//                        System.err.println(buster);
-//                        System.out.println("MOVE 8000 4500"); // MOVE x y | BUST id | RELEASE
-                        discoverMove(buster);
-                        continue;
-                    }
-                    Integer dist = getDist(buster, ghost);
-                    if (ghost.vis && dist <= BUST_RANGE_MAX && dist >= BUST_RANGE_MIN && ghost.value == 0) {
-                        System.err.println("Nearest is " + ghost.id + " " + getDist(buster, ghost));
-                        System.out.println("BUST " + ghost.id);
-//                        System.out.println("BUST " + ghost.id);
-                        System.err.println("BUST WRITTEN!!!");
-                        lastBusted = ghost.id;
-                        ghost.value += 1;
-                        ghosts.remove(ghost.id);
-                        continue;
-                    } else {
-                        System.err.println("Last move to" + ghost.x + " " + ghost.y);
-                        moveTo(ghost);
-//                        ghost.value += 1;
-                        continue;
-                    }
-                }
-                // Write an action using System.out.println()
-                // To debug: System.err.println("Debug messages...");
+        }
+    }
+
+    private static String getBusterAction(Integer busterKey, Coord myBaseCoords) {
+        Buster buster = busters.get(busterKey);
+        if (buster.stunCD > 0) {
+            buster.stunCD--;
+        }
+        checkDiscovery(buster.x, buster.y);
+        if (buster.state == 2) {
+            return moveTo(new Coord(0, 0));
+        }
+        if (buster.state == 1) {
+            Integer distBase = getDist(buster, myBaseCoords);
+            System.err.println("DistBase " + distBase);
+            if (distBase <= BASE_COLLECT_RANGE) {
+                return "RELEASE";
+            } else {
+                System.err.println("Move to base " + myBaseCoords);
+                return "MOVE " + myBaseCoords.x + " " + myBaseCoords.y + " " + busterKey;
             }
-            System.err.println("Moved for " + i);
+        }
+        for (Buster enemyBuster : enemyBusters.values()) {
+            if (enemyBuster.vis && getDist(buster, enemyBuster) <= STUN_RANGE && enemyBuster.hasGhost()) {
+                buster.stunCD = 20;
+                return "STUN " + enemyBuster.id;
+            }
+        }
+        if (ghosts.isEmpty()) {
+            return discoverMove(buster);
+        } else {
+            Ghost ghost = getNextGhost(buster, ghosts, true, discoveredAll ? 20 : 4);
+            if (ghost == null) {
+                return discoverMove(buster);
+            }
+            Integer dist = getDist(buster, ghost);
+            if (ghost.vis && dist <= BUST_RANGE_MAX && dist >= BUST_RANGE_MIN) {
+                System.err.println("Nearest is " + ghost.id + " " + dist);
+                buster.onGhost = ghost.id;
+                return "BUST " + ghost.id;
+            } else {
+                if (dist < BUST_RANGE_MIN) {
+                    System.err.println("To close to ghost!");
+                }
+                return moveTo(ghost);
+            }
         }
     }
 
     private static void filloutDiscoveryList() {
+        discoveredAll = true;
         for (int x = 0; x < (SIZE_X / FIELD_SIZE + 1); x++) {
             for (int y = 0; y < (SIZE_Y / FIELD_SIZE + 1); y++) {
                 int xC = x * FIELD_SIZE, yC = y * FIELD_SIZE;
                 if (!(x == 0 && y == 0 | x == (SIZE_X / FIELD_SIZE) && y == (SIZE_Y / FIELD_SIZE))
-                    && xC < (SIZE_X - 1000) && xC > (1000) && yC > 1000 && yC < (SIZE_Y - 1000)) {
+                        && xC < (SIZE_X - 800) && xC > (800) && yC > 400 && yC < (SIZE_Y - 400)) {
                     toDiscover.add(new Coord(x, y));
                 }
             }
@@ -132,22 +120,21 @@ class Player {
         Collections.shuffle(toDiscover);
     }
 
-    private static void moveTo(Coord myBaseCoords) {
-        System.out.println("MOVE " + myBaseCoords.x + " " + myBaseCoords.y);
+    private static String moveTo(Coord myBaseCoords) {
+        return "MOVE " + myBaseCoords.x + " " + myBaseCoords.y;
     }
 
-    private static Ghost getNextGhost(Buster buster, HashMap<Integer, Ghost> ghosts, boolean visOnly) {
+    private static Ghost getNextGhost(Buster buster, HashMap<Integer, Ghost> ghosts, boolean visOnly, int maxHp) {
         Integer currId = -1;
         Integer dist = Integer.MAX_VALUE;
         for (Map.Entry<Integer, Ghost> ghostEntry : ghosts.entrySet()) {
             Integer ghostId = ghostEntry.getKey();
             Ghost ghost = ghostEntry.getValue();
-            if (ghost.value > 0 || (!ghost.vis && visOnly)) {
-                System.err.println(ghost);
+            if ((!ghost.vis && visOnly) || bustersOnGhost(ghost.id) > 2 || maxHp <= ghost.hp) {
                 continue;
             }
             Integer newDist = getDist(buster, ghost);
-            if (newDist < dist) {
+            if (newDist < dist && newDist > BUST_RANGE_MIN) {
                 currId = ghostId;
                 dist = newDist;
             }
@@ -155,22 +142,31 @@ class Player {
         return ghosts.get(currId);
     }
 
-    private static void discoverMove(Buster buster) {
+    private static int bustersOnGhost(Integer ghostId) {
+        int bustersOnGhost = 0;
+        for (Buster buster : busters.values()) {
+            if (ghostId.equals(buster.onGhost)) {
+                bustersOnGhost++;
+            }
+        }
+        return bustersOnGhost;
+    }
+
+    private static String discoverMove(Buster buster) {
         if (toDiscover.isEmpty()) {
             filloutDiscoveryList();
-            moveTo(new Coord(SIZE_X / 2, SIZE_Y / 2));
+            return moveTo(new Coord(SIZE_X / 2, SIZE_Y / 2));
         } else if (buster.discoverMove != null) {
-            moveTo(new Coord(buster.discoverMove.x * FIELD_SIZE, buster.discoverMove.y * FIELD_SIZE));
+            return moveTo(new Coord(buster.discoverMove.x * FIELD_SIZE, buster.discoverMove.y * FIELD_SIZE));
         } else {
             Coord next = toDiscover.get(0);
             toDiscover.remove(0);
             buster.discoverMove = next;
-            moveTo(new Coord(next.x * FIELD_SIZE, next.y * FIELD_SIZE));
+            return moveTo(new Coord(next.x * FIELD_SIZE, next.y * FIELD_SIZE));
         }
     }
 
     private static void checkDiscovery(int x, int y) {
-//        System.err.println("Before" + toDiscover.size());
         for (Buster buster : busters.values()) {
             if (buster.discoverMove != null && buster.discoverMove.x.equals(new Integer(x / FIELD_SIZE)) && buster.discoverMove.y.equals(new Integer(y / FIELD_SIZE))) {
                 buster.discoverMove = null;
@@ -178,34 +174,32 @@ class Player {
         }
         toDiscover.removeIf((Coord t) -> {
             if (t.x.equals(new Integer(x / FIELD_SIZE)) && t.y.equals(new Integer(y / FIELD_SIZE))) {
-                System.err.println("Removed!");
                 return true;
             }
             return false;
         });
-//        System.err.println("After" + toDiscover.size());
     }
 
     private static void setEntity(int entityId, int x, int y, int entityType, int state, int value) {
         if (entityType == -1) {
-            setGhost(entityId, x, y, value);
+            setGhost(entityId, x, y, state, value);
         } else if (entityType == myTeamId) {
-            System.err.println("Read B " + state + " " + value);
             setBuster(busters, entityId, x, y, state, value);
         } else {
             setBuster(enemyBusters, entityId, x, y, state, value);
         }
     }
 
-    private static void setGhost(int entityId, int x, int y, int value) {
+    private static void setGhost(int entityId, int x, int y, int state, int value) {
         if (ghosts.containsKey(entityId)) {
             Ghost ghost = ghosts.get(entityId);
             ghost.x = x;
             ghost.y = y;
             ghost.value = value;
+            ghost.hp = state;
             ghost.vis = true;
         } else {
-            Ghost ghost = new Ghost(entityId, value, x, y);
+            Ghost ghost = new Ghost(entityId, value, state, x, y);
             ghost.vis = true;
             ghosts.put(entityId, ghost);
         }
@@ -228,13 +222,14 @@ class Player {
 
     public static class Ghost extends Coord {
 
-        int id, value;
+        int id, value, hp;
         boolean vis = false;
 
-        public Ghost(int id, int value, Integer x, Integer y) {
+        public Ghost(int id, int value, int state, Integer x, Integer y) {
             super(x, y);
             this.id = id;
             this.value = value;
+            this.hp = state;
         }
 
         @Override
@@ -246,8 +241,8 @@ class Player {
 
     public static class Buster extends Coord {
 
-        Integer id, state, value;
-        int stunCD = 0, gotStunned = 0;
+        Integer id, state, value, onGhost = null;
+        int stunCD = 0;
         Coord discoverMove = null;
         boolean vis = false;
 
@@ -263,6 +258,17 @@ class Player {
             return "Buster{" + "id=" + id + ", state=" + state + ", value=" + value + '}';
         }
 
+        public boolean isStunned() {
+            return state == 2;
+        }
+
+        public boolean hasGhost() {
+            return state == 1;
+        }
+
+        public boolean isBustingGhost() {
+            return state == 3;
+        }
     }
 
     public static class Coord {
@@ -297,7 +303,7 @@ class Player {
     static final Integer FIELD_SIZE = 1600;
 
     static final Integer BASE_COLLECT_RANGE = 1600, BUST_RANGE_MIN = 900, BUST_RANGE_MAX = 1760, FOG_DIST = 2200, SIZE_Y = 9000, SIZE_X = 16000;
-    static final Integer STUN_RANGE = 1760;
+    static final Integer STUN_RANGE = 1760, MOVE_RANGE = 800;
 
     static final Coord BASE_ONE = new Coord(0, 0), BASE_TWO = new Coord(16000, 9000);
 
